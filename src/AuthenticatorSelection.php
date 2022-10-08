74a55aaa8e12343e30877b77e69c10d6ed196015
Jakob Bennemann <jakob@raidboxes.de>
2022-10-08 19:07:28 +0200

    wip

--- src/AuthenticatorSelection.php
+++ src/AuthenticatorSelection.php
@@ -5,7 +5,7 @@ namespace Jkbennemann\Webauthn;
 class AuthenticatorSelection
 {
     public string $residentKeyType;
-    public string $authenticatorAttachment = 'cross-platform';
+    public ?string $authenticatorAttachment = 'cross-platform';
 
     public function __construct(public string $userVerification, public bool $requiresResidentKey, ?bool $crossPlatform)
     {
